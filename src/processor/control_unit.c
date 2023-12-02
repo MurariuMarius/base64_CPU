3b183d9e28c52d50a4ab02a35552bb5c9ff20ef7
ShadyRaccoon <93075030+ShadyRaccoon@users.noreply.github.com>
2023-12-02 17:32:50 +0200

    Update control_unit.c
    STACK OPS, IMD VAL OPS AND BREAK OPS

--- src/processor/control_unit.c
+++ src/processor/control_unit.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
+#include "opCodes.h"
 #include "internals.h"
-#include "opcodes.h"
 
-
-signal next;
-signal bra;
-signal jmp;
-signal branch_other;
+uint2_t stackOP;     
+signal aluINSTR;   
+signal br_oth;      
+signal bra;         
+signal ABS;         
+signal next;        
 signal end;
-uint2_t spOp;
-
 
 // Test
 static void mock_PSH_op() {
@@ -25,25 +23,43 @@ static void mock_POP_op() {
     printf("POP %d\n", load(SP_operation()));
 }
 
-void CU_handleNextInstruction(uint6_t opCode) {
-    switch (opCode.val) {
-    case HLT:
-        end = ACTIVE;
-        break;
 
-    // TODO
+void CU_handleNextInstruction(uint6_t instruction){
+    next.active = 0b0;
+    
+    //verificare semnal pt operatii pe stack
+    switch(instruction.val){
+        case PSH:
+            stackOP.val = 0b01;
+            mock_PSH_op();
+            break;
+        case POP: 
+            stackOP.val = 0b00;
+            mock_POP_op();
+            break;
+        case HLT:
+            end.active = 0b1;
+            break;
+        default:
+            break;
+    }
 
-    case PSH:
-        spOp.val = 1U;
-        mock_PSH_op();
-        break;
+    //verificare semnale de branch
+    if(instruction.val >= BRZ && instruction.val <= RET){
+        br_oth.active = 0b1;
+        if(instruction.val == BRA){
+            ABS.active = 0b1;
+        }else{
+            ABS.active = 0b0;
+        }
+        next.active = 0b0;
+    }else{
+        br_oth.active = 0b0;
+        next.active = 0b1;
+    }
 
-    case POP:
-        spOp.val = 2U;
-        mock_POP_op();
-        break;
-
-    default:
-        break;
+    //
+    if(instruction.val >= ADDI && instruction.val <= TSTI){ 
+        aluINSTR.active = 0b1;
     }
 }
