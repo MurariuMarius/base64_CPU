b0f99a556e9379995988e3ae26d9d7d277fffbff
MurariuMarius <mam.murariu@gmail.com>
2023-12-14 11:02:51 +0200

    Fixed shift operations and carry generation

--- in.asm
+++ in.asm
@@ -1,15 +1,2 @@
-MOV X, #4
-PSH Y   ; preserve the value of Y
-PSH X   ; pass X as parameter
-JMP func
-POP X   ; store in X the return value
-POP Y   ; retreive the original value of Y
-MOV Y, X
-MUL X, X
-HLT
-func: LDR Y, [SP+1] ; access the function parameter through the stack
-STR Y, [Y-1]
-ADD Y, #10
-STR Y, [SP+1] ; store the return value in the stack
-LDR Y, [Y-11]
-RET
+MOV X, #255
+NOT Y, X
--- src/processor/ALU_flags.c
+++ src/processor/ALU_flags.c
@@ -108,113 +108,100 @@ uint16_t main_ALU_fcn()
         {
         case ADD:
         case ADDI:
-            // perform the addition operation
-            // check for CF
             if ((uint32_t)(opA + opB) > UINT16_MAX)
                 CF = ACTIVE;
             accumulator = opA + opB;
             break;
         case SUB:
         case SUBI:
-            // perform the subtraction operation
-            // check for CF
+
             if (opA < opB)
                 CF = ACTIVE;
             accumulator = opA - opB;
             break;
         case LSR:
         case LSRI:
-            // perform the logical shift right operation
-            accumulator = opA >> opB;
-            // check for carry
-            if (opA & 0b01)
-                CF = ACTIVE;
+            accumulator = opA;
+            for (uint8_t i = 0; i < opB; i++) {
+                CF.active = (accumulator & 1);
+                accumulator >>= 1;
+                accumulator &= 0x7FFF;
+            }
             break;
-        case LSL: // these are the same
+        case LSL:
         case LSLI:
         case RSL:
         case RSLI:
-            // perfrom the logical shift left operation
-            accumulator = opA << opB;
+            accumulator = opA;
+            for (uint8_t i = 0; i < opB; i++) {
+                CF.active = NEGATIVE(accumulator);
+                accumulator <<= 1;
+            }
             break;
         case RSR:
         case RSRI:
-            // check for carry
-            if (opA & 0b01)
-                CF = ACTIVE;
-            if (opA & (1 << 15)) // if opA's MSB == 1
-            {
-                accumulator = opA >> opB;
-                while (opB)
-                {
-                    opB--;
-                    accumulator |= (1 << (15 - opB)); // fill the first opB positions in result with opA's MSB
-                }
+            accumulator = opA;
+
+            signal sign = (signal){ NEGATIVE(opA) };
+
+            for (uint8_t i = 0; i < opB; i++) {
+                CF.active = (accumulator & 1);
+                accumulator <<= 1;
+                accumulator |= sign.active << 15;
             }
-            else
-                accumulator = opA >> opB; // first opB positions in result are filled with 0
             break;
         case MOV:
-            // perform the MOV operation
             accumulator = opB;
             break;
         case MUL:
         case MULI:
-            // perform the MUL operation
             accumulator = opA * opB;
             break;
         case DIV:
         case DIVI:
-            // perform the DIV operation
             if (opB)
                 accumulator = opA / opB;
             break;
         case MOD:
         case MODI:
-            // perform the MOD operation
             if (opB)
                 accumulator = opA % opB;
             break;
         case AND:
         case ANDI:
-            // perform the AND operation
             accumulator = opA & opB;
             break;
         case OR:
         case ORI:
-            // perform the OR operation
             accumulator = opA | opB;
             break;
         case XOR:
         case XORI:
-            // perform the XOR operation
             accumulator = opA ^ opB;
             break;
         case NOT:
         case NOTI:
-            // perform the NOT operation on the first operand
             accumulator = ~opB;
             break;
-        case TST: // these are the same
-        case TSTI: // these are the same
+        case TST:
+        case TSTI:
         case CMP:
         case CMPI:
-            // load into result the difference...flags are set after switch
             NF.active = (opA - opB < 0);
             ZF.active = (opA == opB);
             accumulator = opA;
             break;
         case INCI:
-            // perform the INC operation on the first operand
             accumulator = opA + 1;
             break;
         case DECI:
-            // perform the DEC operation on the first operand
             accumulator = opA - 1;
             break;
         default:
-            // if the opcode is not recognised, return -1
             accumulator = -1;
+            ZF = ACTIVE;
+            NF = ACTIVE;
+            return accumulator;
         }
     }
 
