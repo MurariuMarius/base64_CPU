2cc4414e4f3229495d22dc0b3f9ca3bb7a66103b
MurariuMarius <mam.murariu@gmail.com>
2023-12-15 08:58:42 +0200

    Find the nth term of the Fibonacci sequence

--- in.asm
+++ in.asm
@@ -1,2 +1,34 @@
-MOV X, #255
-NOT Y, X
+MOV X, #15
+PSH Y
+PSH X
+JMP fib
+POP X
+POP Y
+ADD X, #0
+HLT
+fib: LDR X, [SP+1] ; acces argument prin stiva
+
+CMP X, #2 ; if X <= 1
+BRN end ; caz de baza
+
+rec: MOV Y, #0 ; initializare variabila pentru suma
+
+DEC X ; preagatire X - 1
+
+PSH X ; pastreaza X
+PSH X
+JMP fib
+POP Y ; Y = fib(X - 1)
+POP X
+
+DEC X ; pregatire X - 2
+
+PSH Y ; pastreaza Y
+PSH X
+JMP fib
+POP X  ; X = fib(X - 2)
+POP Y
+
+ADD X, Y ; X = fib(X - 1) + fib(X - 2)
+end: STR X, [SP+1] ; retureaza X prin stiva
+RET
